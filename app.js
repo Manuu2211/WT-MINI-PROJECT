ce130fac3b9b4f74d73f0208668571cbe4a9c4be
Manuu2211 <125660502+Manuu2211@users.noreply.github.com>
2023-06-01 15:40:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function talk(){
+    var know = {
+    "hii":"hello how can i help you",
+    "How are you" : "Good :)",
+    "what can you do for mee":"i'll try my best to fullfill your demand you just say what you want",
+    "What can i do for you" : "Please Give us A Follow & Like.",
+    "Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
+    "ok" : "Thank You So Much ",
+    "Bye" : "Okay! Will meet soon.."
+    };
+    var user = document.getElementById('userBox').value;
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+    if (user in know) {
+    document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    }else{
+    document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
+    }
+    }
